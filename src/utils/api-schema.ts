2587bc964855eeda556f18f52b312d5aae985d6d
Tommy Bjerregaard <tommy@designsystems.international>
2024-11-27 13:58:27 +0100

    Moved schema to always use the content folder

--- src/utils/api-schema.ts
+++ src/utils/api-schema.ts
@@ -1,16 +1,11 @@
 import SwaggerParser from '@apidevtools/swagger-parser';
 import { unstable_cacheLife as cacheLife } from 'next/cache';
 
-import generatedSchema from '../generated/api-schema.json';
-
-const isDevelopment = process.env.NODE_ENV === 'development';
-
 export async function getApiSchema() {
   'use cache';
   cacheLife('days');
 
-  // In development mode use the generated schema to avoid fetching to have faster compile times
-  const schema = isDevelopment ? generatedSchema : await SwaggerParser.parse(process.env.CLOUDSMITH_API_URL!);
+  const schema = await SwaggerParser.parse(`src/content/api-schema.json`);
 
   if (!schema) {
     throw new Error('Failed to fetch API schema');
