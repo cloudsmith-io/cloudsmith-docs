24b06acda44af3b884fa67fb48040173617fc559
Tommy Bjerregaard <tommy@designsystems.international>
2024-12-05 09:09:00 +0100

    Added fonts from old repo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+import { cx } from 'class-variance-authority';
+
+import { mdSystem, replica, replicaMono } from './_assets/fonts';
+
 import './global.css';
 import './variables.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cx(mdSystem.variable, replica.variable, replicaMono.variable)}>
       <body>{children}</body>
     </html>
   );
