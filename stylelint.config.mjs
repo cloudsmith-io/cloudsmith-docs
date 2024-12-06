6321885448fa089ec1f9507661889acff3290147
Tommy Bjerregaard <tommy@designsystems.international>
2024-12-06 09:28:22 +0100

    Added reset styles

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,41 +1,45 @@
-{
-  "extends": ["stylelint-config-standard"],
-  "rules": {
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard"],
+  rules: {
     "at-rule-no-unknown": true,
     "at-rule-no-vendor-prefix": true,
     "block-no-empty": true,
     "color-named": "never",
     "color-no-invalid-hex": true,
+    "rule-empty-line-before": false,
     "declaration-block-no-duplicate-properties": true,
     "declaration-empty-line-before": [
       "always",
-      { "except": ["first-nested"], "ignore": ["after-declaration"] }
+      { except: ["first-nested", "after-comment"], ignore: ["after-declaration"] }
     ],
     "declaration-no-important": true,
     "declaration-property-value-disallowed-list": { "/^border/": ["none"] },
     "function-url-quotes": "always",
-    "function-no-unknown": [true, { "ignoreFunctions": ["fluid"] }],
+    "function-no-unknown": [true, { ignoreFunctions: ["fluid"] }],
     "max-nesting-depth": 3,
     "media-feature-name-no-vendor-prefix": true,
     "no-duplicate-selectors": true,
     "property-no-vendor-prefix": true,
-
     "selector-class-pattern": [
       "^[a-z][a-zA-Z0-9]+$",
-      { "message": "Expected class selector to be camelCase" }
+      { message: "Expected class selector to be camelCase" }
     ],
-
     "keyframes-name-pattern": [
       "^[a-z][a-zA-Z0-9]+$",
-      { "message": "Expected keyframe name to be camelCase" }
+      { message: "Expected keyframe name to be camelCase" }
     ],
-
     "selector-max-id": 0,
     "selector-no-qualifying-type": true,
     "selector-no-vendor-prefix": true,
     "shorthand-property-no-redundant-values": true,
-    "value-no-vendor-prefix": true
+    "value-no-vendor-prefix": true,
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        "ignorePseudoClasses": ["global"]
+      }
+    ],
   },
-
-  "ignoreFiles": ["node_modules/**/*.css"]
-}
+  ignoreFiles: ["node_modules/**/*.css"]
+};
