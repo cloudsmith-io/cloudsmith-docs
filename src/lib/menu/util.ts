7160b23ff3d98dfa199da4cf4cb406f851f1cde2
Rune Madsen <rune@runemadsen.com>
2024-12-11 09:37:50 +0100

    remove API sidebar

--- src/lib/menu/util.ts
+++ src/lib/menu/util.ts
@@ -2,7 +2,7 @@ import type { Menu, MenuItem } from './types';
 
 import json from '../../content/menu.json';
 
-const menu: Menu = json;
+const menu: Menu = json as Menu;
 
 /**
  * Return a single top-level menu section based on the key
