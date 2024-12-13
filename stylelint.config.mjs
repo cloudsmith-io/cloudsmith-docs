c7b5c067d85b5e855f36f8f6b2292f10f929c069
Tommy Bjerregaard <tommy@designsystems.international>
2024-12-13 13:13:18 +0100

    Running the stylelint fix
    Because we had some errors in the config file

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,39 +1,39 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ["stylelint-config-standard"],
+  extends: ['stylelint-config-standard'],
   rules: {
-    "at-rule-no-unknown": true,
-    "at-rule-no-vendor-prefix": true,
-    "block-no-empty": true,
-    "color-named": "never",
-    "color-no-invalid-hex": true,
-    "rule-empty-line-before": false,
-    "declaration-block-no-duplicate-properties": true,
-    "declaration-empty-line-before": [
-      "always",
-      { except: ["first-nested", "after-comment"], ignore: ["after-declaration"] }
+    'at-rule-no-unknown': true,
+    'at-rule-no-vendor-prefix': true,
+    'block-no-empty': true,
+    'color-named': 'never',
+    'color-no-invalid-hex': true,
+    'rule-empty-line-before': [
+      'always-multi-line',
+      {
+        except: ['first-nested'],
+        ignore: ['after-comment'],
+      },
     ],
-    "declaration-no-important": true,
-    "declaration-property-value-disallowed-list": { "/^border/": ["none"] },
-    "function-url-quotes": "always",
-    "function-no-unknown": [true, { ignoreFunctions: ["fluid"] }],
-    "max-nesting-depth": 3,
-    "media-feature-name-no-vendor-prefix": true,
-    "no-duplicate-selectors": true,
-    "property-no-vendor-prefix": true,
-    "selector-class-pattern": [
-      "^[a-z][a-zA-Z0-9]+$",
-      { message: "Expected class selector to be camelCase" }
+    'declaration-block-no-duplicate-properties': true,
+    'declaration-empty-line-before': [
+      'always',
+      { except: ['first-nested', 'after-comment'], ignore: ['after-declaration'] },
     ],
-    "keyframes-name-pattern": [
-      "^[a-z][a-zA-Z0-9]+$",
-      { message: "Expected keyframe name to be camelCase" }
-    ],
-    "selector-max-id": 0,
-    "selector-no-qualifying-type": true,
-    "selector-no-vendor-prefix": true,
-    "shorthand-property-no-redundant-values": true,
-    "value-no-vendor-prefix": true,
+    'declaration-no-important': true,
+    'declaration-property-value-disallowed-list': { '/^border/': ['none'] },
+    'function-url-quotes': 'always',
+    'function-no-unknown': [true, { ignoreFunctions: ['fluid'] }],
+    'max-nesting-depth': 3,
+    'media-feature-name-no-vendor-prefix': true,
+    'no-duplicate-selectors': true,
+    'property-no-vendor-prefix': true,
+    'selector-class-pattern': ['^[a-z][a-zA-Z0-9]+$', { message: 'Expected class selector to be camelCase' }],
+    'keyframes-name-pattern': ['^[a-z][a-zA-Z0-9]+$', { message: 'Expected keyframe name to be camelCase' }],
+    'selector-max-id': 0,
+    'selector-no-qualifying-type': true,
+    'selector-no-vendor-prefix': true,
+    'shorthand-property-no-redundant-values': true,
+    'value-no-vendor-prefix': true,
   },
-  ignoreFiles: ["node_modules/**/*.css"]
+  ignoreFiles: ['node_modules/**/*.css'],
 };
