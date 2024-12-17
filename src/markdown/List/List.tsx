2a9e4600e6c36551e540db25d86670ec33d5b8f2
Tommy Bjerregaard <tommy@designsystems.international>
2024-12-17 11:08:36 +0100

    List markdown component (#19)
    This is just a small PR adding some styling on the UL and OL elements in markdown. I also adjusted some spacings in some of the other markdown components.

---
+++ src/markdown/List/List.tsx
@@ -0,0 +1,25 @@
+import { cx } from 'class-variance-authority';
+
+import styles from './List.module.css';
+
+export function List(props: ListProps) {
+  if (isOrderedList(props)) {
+    const {
+      ordered, // eslint-disable-line @typescript-eslint/no-unused-vars
+      ...rest
+    } = props;
+    return <ol className={cx(styles.root, styles.ordered)} {...rest} />;
+  }
+
+  return <ul className={cx(styles.root, styles.unOrdered)} {...props} />;
+}
+
+function isOrderedList(list: ListProps): list is OrderedLists {
+  return (list as OrderedLists).ordered;
+}
+
+type ListProps = UnorderedLists | OrderedLists;
+type UnorderedLists = React.ComponentPropsWithoutRef<'ul'>;
+type OrderedLists = React.ComponentPropsWithoutRef<'ol'> & {
+  ordered: true;
+};
