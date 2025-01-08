809a869006e9e5c550ba8865280c6f4eacd3b82f
david s <david@super45.cl>
2025-01-08 15:00:13 -0300

    Adds Arrow icon to registry

--- src/icons/icon-registry.tsx
+++ src/icons/icon-registry.tsx
@@ -2,8 +2,10 @@ import { createIconRegistry } from './util/create-icon-registry';
 
 import { CloseIcon } from './svgs/Close';
 import { ChevronDownIcon } from './svgs/ChevronDown';
+import { ArrowIcon } from './svgs/Arrow';
 
 export const iconRegistry = createIconRegistry({
   close: CloseIcon,
   chevronDown: ChevronDownIcon,
+  arrow: ArrowIcon,
 });
