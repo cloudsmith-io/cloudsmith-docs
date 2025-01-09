2ba6ccf6017c53ecb515372e6fb2aca2b2d7a7ac
david s <david@super45.cl>
2025-01-09 16:34:31 -0300

    adds Card icons to the icon registry

--- src/icons/icon-registry.tsx
+++ src/icons/icon-registry.tsx
@@ -3,9 +3,15 @@ import { createIconRegistry } from './util/create-icon-registry';
 import { CloseIcon } from './svgs/Close';
 import { ChevronDownIcon } from './svgs/ChevronDown';
 import { ArrowIcon } from './svgs/Arrow';
+import { DocumentationIcon } from './svgs/symbols/Documentation';
+import { GuideIcon } from './svgs/symbols/Guide';
+import { ApiReferenceIcon } from './svgs/symbols/ApiReference';
 
 export const iconRegistry = createIconRegistry({
   close: CloseIcon,
   chevronDown: ChevronDownIcon,
   arrow: ArrowIcon,
+  documentation: DocumentationIcon,
+  guide: GuideIcon,
+  apiReference: ApiReferenceIcon,
 });
