73d127fcbee53a5e50558c5b2c5b96a967d9d366
Tommy Bjerregaard <tommy@designsystems.international>
2025-01-10 14:39:43 +0100

    Implemented all variants of methods based on figma UI

--- src/components/Method/Method.tsx
+++ src/components/Method/Method.tsx
@@ -1,22 +1,24 @@
 import { VariantProps, cva } from 'class-variance-authority';
+import { OpenAPIV3 } from 'openapi-types';
 
 import styles from './Method.module.css';
 
-const method = cva(styles.root, {
+const methodVariants = cva(styles.root, {
   variants: {
     size: {
       small: styles.small,
       medium: styles.medium,
     },
     type: {
-      get: styles.get,
-      put: styles.put,
-      post: styles.post,
-      delete: styles.delete,
-      options: styles.options,
-      head: styles.head,
-      patch: styles.patch,
-      trace: styles.trace,
+      package: styles.package,
+      status: styles.status,
+    },
+    variant: {
+      neutral: styles.neutral,
+      green: styles.green,
+      yellow: styles.yellow,
+      red: styles.red,
+      grey: styles.grey,
     },
     active: {
       true: styles.active,
@@ -24,20 +26,50 @@ const method = cva(styles.root, {
   },
   defaultVariants: {
     size: 'small',
+    type: 'package',
   },
 });
 
-export function Method({ type, active, size, ...rest }: Method.Props) {
-  return <div className={method({ type, active, size })} {...rest} />;
+export function Method({ size, type, ...props }: Method.Props) {
+  if ('method' in props) {
+    const { method, ...rest } = props;
+
+    // Map the HttpMethods to the color variant for easier consumption
+    const methods: { [key in Method.HttpMethods]: Method.VariantsProps['variant'] } = {
+      get: 'neutral',
+      put: 'neutral',
+      post: 'green',
+      delete: 'red',
+      options: 'yellow',
+      head: 'neutral',
+      patch: 'neutral',
+      trace: 'neutral',
+    };
+
+    return <div className={methodVariants({ size, type, variant: methods[method] })} {...rest} />;
+  }
+
+  const { variant, ...rest } = props;
+
+  return <div className={methodVariants({ size, type, variant })} {...rest} />;
 }
 
 export namespace Method {
-  type VariantsProps = VariantProps<typeof method>;
+  export type VariantsProps = VariantProps<typeof methodVariants>;
+  export type HttpMethods = Lowercase<keyof typeof OpenAPIV3.HttpMethods>;
 
-  export interface Props
-    extends React.ComponentPropsWithoutRef<'div'>,
-      Omit<VariantsProps, 'type'>,
-      Required<Pick<VariantsProps, 'type'>> {
+  type DivElement = React.ComponentPropsWithoutRef<'div'>;
+
+  interface withMethod extends DivElement, Omit<VariantsProps, 'variant'> {
+    method: HttpMethods;
+  }
+
+  interface withVariant
+    extends DivElement,
+      Omit<VariantsProps, 'variant'>,
+      Required<Pick<VariantsProps, 'variant'>> {
     children: React.ReactNode;
   }
+
+  export type Props = withMethod | withVariant;
 }
--- src/components/RequestResponse/RequestResponse.tsx
+++ src/components/RequestResponse/RequestResponse.tsx
@@ -9,11 +9,18 @@ export const RequestResponse = (operation: PropsRequestResponseProps) => {
   return (
     <div className={styles.root}>
       <Heading size="h2">Request</Heading>
+
+      <Method method="delete" type="status" size="medium">
+        get
+      </Method>
+
+      <Method variant="green" type="status" size="medium">
+        get
+      </Method>
+
       <div className={styles.request}>
         <div className={styles.url}>
-          <Method type={operation.method} size="medium">
-            {operation.method}
-          </Method>
+          <Method method={operation.method} type="status" size="medium" />
           {`${process.env.NEXT_PUBLIC_CLOUDSMITH_API_URL}${operation.path}`}
         </div>
 
@@ -21,6 +28,22 @@ export const RequestResponse = (operation: PropsRequestResponseProps) => {
           <div className={cx(styles.item, styles.header)}>
             <div className={styles.subItem}>Path params</div>
           </div>
+          {operation.parameters
+            // Only get path parameters
+            ?.filter((param) => 'in' in param)
+            .map((param) => (
+              <div key={param.name} className={styles.item}>
+                <div className={styles.subItem}>{param.name}</div>
+                <div className={styles.subItem}>
+                  {typeof param.schema === 'object' ? param.schema.type : param.schema}
+                </div>
+                <div className={styles.subItem}>
+                  <Method variant={param.required ? 'red' : 'grey'} type="status" size="medium">
+                    {param.required ? 'required' : 'optional'}
+                  </Method>
+                </div>
+              </div>
+            ))}
           <div className={styles.item}>
             <div className={styles.subItem}>owner</div>
             <div className={styles.subItem}>string</div>
