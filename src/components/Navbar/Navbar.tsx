e771bbab7752f95dad342589653d0de3227eb0ae
david s <david@super45.cl>
2025-01-11 01:35:59 -0300

    adds navbar styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,69 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { cx } from 'class-variance-authority';
 import Link from 'next/link';
-import { Container } from '../Container';
-import Image from 'next/image';
+import { Container, Flex } from '@/components';
+import { LogoWordMark, LogoSymbol } from '@/components/Logo';
+import { Icon } from '@/icons';
 
 import styles from './Navbar.module.css';
 
 export const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname === '/');
+
   return (
-    <div className={styles.root}>
+    <div className={cx(styles.root, pathname === '/' && styles.homeNavbar)}>
       <Container className={styles.container}>
         <Link href="/" className={styles.logo}>
-          <Image src="/images/cloudsmith-logo.svg" alt="Cloudsmith" width={164} height={25} />
+          <LogoWordMark className={styles.logoWordmark} />
+          <LogoSymbol className={styles.logoSymbol} />
         </Link>
-        <nav className={styles.nav}>
-          <Link href="/documentation">Documentation</Link>
-          <Link href="/guides">Guides</Link>
-          <Link href="/api">API Reference</Link>
-        </nav>
+
+        <span className={styles.currentSection}>
+          <Icon name="docs/link" aria-hidden="true" focusable="false" />
+          <span>Documentation</span>
+        </span>
+
+        <Flex gap="m" justify="between" wrap={false} className={cx(styles.navContainer)}>
+          <nav className={styles.nav}>
+            <Link href="/documentation" className={styles.navLink}>
+              <Icon name="docs/documentation" aria-hidden="true" focusable="false" />
+              Documentation
+            </Link>
+            <Link href="/guides" className={styles.navLink}>
+              <Icon name="docs/guide" aria-hidden="true" focusable="false" />
+              Guides
+            </Link>
+            <Link href="/api" className={styles.navLink}>
+              <Icon name="docs/api" aria-hidden="true" focusable="false" />
+              API Reference
+            </Link>
+          </nav>
+
+          <button aria-label="Search" className={styles.searchButton}>
+            <Icon
+              name="search"
+              aria-hidden="true"
+              focusable="false"
+              width={10}
+              height={10}
+              viewBox="0 0 10 10"
+              className={styles.searchIcon}
+            />
+            <div className={styles.searchButtonText}>
+              <span>Search</span>
+              <kbd>
+                <abbr title="Command">⌘</abbr> K
+              </kbd>
+            </div>
+          </button>
+
+          <button className={styles.menuButton} aria-label="Menu">
+            <Icon name="menu" aria-hidden="true" focusable="false" />
+          </button>
+        </Flex>
       </Container>
     </div>
   );
