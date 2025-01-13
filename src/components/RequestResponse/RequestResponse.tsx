e2635e8b6b99e66b74cba7e4b35722a8e59f77eb
Tommy Bjerregaard <tommy@designsystems.international>
2025-01-13 09:39:36 +0100

    Operations Request UI with Tag component (#31)
    This PR is the first PR in a series of PR's where im adding UI for rendering the operations in the API docs.
    
    - Adjusted and refactored `<Method>` component to instead be called `<Tag>`. This now have the correct colors and props, but we still need to decide on where to put the color variables since they are not generel.
    - Added styling/UI for Request path params.
    
    <img width="990" alt="image" src="https://github.com/user-attachments/assets/ac33ec2e-1b54-4c4b-b1a9-1c87fdd1848c" />

---
+++ src/components/RequestResponse/RequestResponse.tsx
@@ -0,0 +1,49 @@
+import { cx } from 'class-variance-authority';
+import { ApiOperation } from '@/lib/swagger/types';
+
+import styles from './RequestResponse.module.css';
+import { Heading } from '@/markdown';
+import { Tag } from '../Tag';
+
+export const RequestResponse = (operation: PropsRequestResponseProps) => {
+  return (
+    <div className={styles.root}>
+      <Heading size="h2">Request</Heading>
+
+      <div className={styles.request}>
+        <div className={styles.url}>
+          <Tag method={operation.method} />
+          {`${process.env.NEXT_PUBLIC_CLOUDSMITH_API_URL}${operation.path}`}
+        </div>
+
+        <div className={styles.grid}>
+          <div className={cx(styles.item, styles.header)}>
+            <div className={styles.subItem}>Path params</div>
+          </div>
+
+          {operation.parameters
+            // Only get path parameters
+            ?.filter((param) => 'in' in param)
+            .map((param) => (
+              <div key={param.name} className={styles.item}>
+                <div className={styles.subItem}>{param.name}</div>
+                <div className={styles.subItem}>
+                  {/* @ts-expect-error" TODO: Find fix */}
+                  {param.schema && typeof param.schema === 'string' ? param.schema : param.schema?.type}
+                </div>
+                <div className={styles.subItem}>
+                  <Tag variant={param.required ? 'red' : 'grey'}>
+                    {param.required ? 'required' : 'optional'}
+                  </Tag>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+
+      <Heading size="h2">Response</Heading>
+    </div>
+  );
+};
+
+type PropsRequestResponseProps = ApiOperation;
