3008cb00e1ad7c7fef1dc92536ab093effeca7b3
Tommy Bjerregaard <tommy@designsystems.international>
2025-01-13 14:28:43 +0100

    Make table subgrid work

--- src/components/RequestResponse/RequestResponse.tsx
+++ src/components/RequestResponse/RequestResponse.tsx
@@ -1,67 +1,78 @@
-import { cx } from 'class-variance-authority';
 import { ApiOperation } from '@/lib/swagger/types';
+import { cx } from 'class-variance-authority';
+import { Heading } from '@/markdown';
+import { OpenAPIV3 } from 'openapi-types';
+import { Tag } from '../Tag';
 
 import styles from './RequestResponse.module.css';
-import { Heading } from '@/markdown';
-import { Tag } from '../Tag';
-import { OpenAPIV3 } from 'openapi-types';
 
 export const RequestResponse = (operation: PropsRequestResponseProps) => {
   return (
-    <div className={styles.root}>
-      <Heading size="h2">Request</Heading>
+    <>
+      <div className={styles.root}>
+        <Heading size="h2" className={styles.fullWidth}>
+          Request
+        </Heading>
 
-      <div className={styles.request}>
-        <div className={styles.url}>
-          <Tag method={operation.method} />
-          {`${process.env.NEXT_PUBLIC_CLOUDSMITH_API_URL}${operation.path}`}
+        <div className={styles.request}>
+          <div className={styles.url}>
+            <Tag method={operation.method} />
+            {`${process.env.NEXT_PUBLIC_CLOUDSMITH_API_URL}${operation.path}`}
+          </div>
         </div>
+
+        <PathParams {...operation} />
+        <QueryParams {...operation} />
+
+        <Heading size="h2">Response</Heading>
       </div>
-
-      <PathParams {...operation} />
-      <QueryParams {...operation} />
-
-      <Heading size="h2">Response</Heading>
-    </div>
+    </>
   );
 };
 
 const PathParams = (operation: PropsRequestResponseProps) => {
-  return (
-    <div className={styles.grid}>
-      <div className={cx(styles.item, styles.header)}>
-        <div className={styles.subItem}>Path params</div>
-      </div>
+  const operations = (operation.parameters as OpenAPIV3.ParameterObject[])?.filter(
+    (param) => param.in === 'path',
+  );
 
-      {(operation.parameters as OpenAPIV3.ParameterObject[])
-        // Only get path parameters
-        ?.filter((param) => param.in === 'path')
-        .map((param) => (
+  if (operations.length) {
+    return (
+      <div className={styles.grid}>
+        <div className={cx(styles.item, styles.header)}>
+          <div className={styles.subItem}>Path params</div>
+        </div>
+
+        {operations.map((param) => (
           <div key={param.name} className={styles.item}>
             <div className={styles.subItem}>{param.name}</div>
-            <div className={styles.subItem}>
-              {param.schema && typeof param.schema === 'string' ? param.schema : param.schema?.type}
+            <div className={cx(styles.subItem, styles.subItemType)}>
+              {(param.schema as OpenAPIV3.SchemaObject).type}
             </div>
             <div className={styles.subItem}>
               <Tag variant={param.required ? 'red' : 'grey'}>{param.required ? 'required' : 'optional'}</Tag>
             </div>
           </div>
         ))}
-    </div>
-  );
+      </div>
+    );
+  }
+
+  return null;
 };
 
 const QueryParams = (operation: PropsRequestResponseProps) => {
-  return (
-    <div className={styles.grid}>
-      <div className={cx(styles.item, styles.header)}>
-        <div className={styles.subItem}>Query params</div>
-      </div>
+  const operations = (operation.parameters as OpenAPIV3.ParameterObject[])?.filter(
+    (param) => param.in === 'query',
+  );
 
-      {(operation.parameters as OpenAPIV3.ParameterObject[])
-        // Only get path parameters
-        ?.filter((param) => param.in === 'query')
-        .map((param) => (
+  if (operations.length) {
+    return (
+      <div className={styles.grid}>
+        <div className={cx(styles.item, styles.header)}>
+          <div className={styles.subItem}>Query params</div>
+        </div>
+
+        {operations.map((param) => (
           <div key={param.name} className={styles.item}>
             <div className={styles.subItem}>{param.name}</div>
             <div className={cx(styles.subItem, styles.subItemType)}>
@@ -70,8 +81,11 @@ const QueryParams = (operation: PropsRequestResponseProps) => {
             <div className={cx(styles.subItem, styles.subItemDescription)}>{param.description}</div>
           </div>
         ))}
-    </div>
-  );
+      </div>
+    );
+  }
+
+  return null;
 };
 
 type PropsRequestResponseProps = ApiOperation;
