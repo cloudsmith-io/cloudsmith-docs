3df69ab52e62e4a9bc603eca63b2213e3325b491
Tommy Bjerregaard <tommy@designsystems.international>
2025-01-16 12:19:04 +0100

    UI for response in API docs (#39)
    This PR is the second PR in a series of PR's where im adding UI for rendering the operations in the API docs.
    
    - Added more UI in the `<RequestResponse>` component
    - Refactored types and props for `<Tag>` component for easier consumption
    - Added README for Tag component
    
    > [!NOTE]
    > Still missing UI for Request body, validation rules and much more. Headline is also not correct, it's not part of the json yet.
    > Currently the whole RequestResponse is one single file, but we could consider move it out into multiple files. This can be done later on. For now a lot of the styles for the tables share same styles making it hard to decide on if moving it into multiple files makes sense.
    
    **Demo**
    ![Screen Recording 2025-01-15 at 14 16 35](https://github.com/user-attachments/assets/803d38ca-57fb-49d1-b180-003ce94df8b8)

--- src/components/RequestResponse/RequestResponse.tsx
+++ src/components/RequestResponse/RequestResponse.tsx
@@ -1,49 +1,253 @@
-import { cx } from 'class-variance-authority';
+'use client';
+
+import { ChevronIcon } from '@/icons/Chevron';
 import { ApiOperation } from '@/lib/swagger/types';
+import { Heading, Paragraph } from '@/markdown';
+import { cx } from 'class-variance-authority';
+import { Transition } from 'motion/dist/react';
+import * as m from 'motion/react-m';
+import { OpenAPIV3 } from 'openapi-types';
+import { useState } from 'react';
+import { Tag } from '../Tag';
 
 import styles from './RequestResponse.module.css';
-import { Heading } from '@/markdown';
-import { Tag } from '../Tag';
+
+const transition: Transition = { duration: 0.35, ease: [0.55, 0, 0, 1] };
 
 export const RequestResponse = (operation: PropsRequestResponseProps) => {
   return (
-    <div className={styles.root}>
-      <Heading size="h2">Request</Heading>
+    <>
+      {/* TODO: Use headline from where? */}
+      <Heading size="h1">Headline</Heading>
 
-      <div className={styles.request}>
-        <div className={styles.url}>
-          <Tag method={operation.method} />
-          {`${process.env.NEXT_PUBLIC_CLOUDSMITH_API_URL}${operation.path}`}
-        </div>
+      <Paragraph>{operation.description}</Paragraph>
 
-        <div className={styles.grid}>
-          <div className={cx(styles.item, styles.header)}>
-            <div className={styles.subItem}>Path params</div>
+      <div className={styles.root}>
+        <Heading size="h2" className={styles.fullWidth}>
+          Request
+        </Heading>
+
+        <div className={styles.request}>
+          <div className={styles.url}>
+            <Tag method={operation.method} />
+            {`${process.env.NEXT_PUBLIC_CLOUDSMITH_API_URL}${operation.path}`}
           </div>
-
-          {operation.parameters
-            // Only get path parameters
-            ?.filter((param) => 'in' in param)
-            .map((param) => (
-              <div key={param.name} className={styles.item}>
-                <div className={styles.subItem}>{param.name}</div>
-                <div className={styles.subItem}>
-                  {/* @ts-expect-error" TODO: Find fix */}
-                  {param.schema && typeof param.schema === 'string' ? param.schema : param.schema?.type}
-                </div>
-                <div className={styles.subItem}>
-                  <Tag variant={param.required ? 'red' : 'grey'}>
-                    {param.required ? 'required' : 'optional'}
-                  </Tag>
-                </div>
-              </div>
-            ))}
         </div>
-      </div>
 
-      <Heading size="h2">Response</Heading>
-    </div>
+        <PathParams {...operation} />
+        <QueryParams {...operation} />
+
+        {/* TODO Request body object */}
+
+        <Heading size="h2">Response</Heading>
+
+        <Responses {...operation} />
+      </div>
+    </>
+  );
+};
+
+const RequiredTag = ({ isRequired }: { isRequired: boolean | undefined }) => (
+  <Tag variant={isRequired ? 'red' : 'grey'}>{isRequired ? 'required' : 'optional'}</Tag>
+);
+
+const PathParams = (operation: PropsRequestResponseProps) => {
+  const operations = (operation.parameters as OpenAPIV3.ParameterObject[])?.filter(
+    (param) => param.in === 'path',
+  );
+
+  if (operations.length) {
+    return (
+      <div className={styles.grid}>
+        <div className={cx(styles.item, styles.header)}>
+          <div className={styles.subItem}>Path params</div>
+        </div>
+
+        {operations.map((param) => (
+          <div key={param.name} className={styles.item}>
+            <div className={styles.subItem}>{param.name}</div>
+            <div className={cx(styles.subItem, styles.subItemType)}>
+              {(param.schema as OpenAPIV3.SchemaObject).type}
+            </div>
+            <div className={styles.subItem}>
+              <RequiredTag isRequired={param.required} />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const QueryParams = (operation: PropsRequestResponseProps) => {
+  const operations = (operation.parameters as OpenAPIV3.ParameterObject[])?.filter(
+    (param) => param.in === 'query',
+  );
+
+  if (operations.length) {
+    return (
+      <div className={styles.grid}>
+        <div className={cx(styles.item, styles.header)}>
+          <div className={styles.subItem}>Query params</div>
+        </div>
+
+        {operations.map((param) => (
+          <div key={param.name} className={styles.item}>
+            <div className={styles.subItem}>{param.name}</div>
+            <div className={cx(styles.subItem, styles.subItemType)}>
+              {(param.schema as OpenAPIV3.SchemaObject).type}
+            </div>
+            <div className={cx(styles.subItem, styles.subItemDescription)}>{param.description}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const Responses = (operation: PropsRequestResponseProps) => {
+  const responses = Object.entries(operation.responses as { [code: string]: OpenAPIV3.ResponseObject });
+
+  if (responses.length) {
+    return (
+      <div className={styles.grid}>
+        <div className={cx(styles.item, styles.header)}>
+          <div className={styles.subItem}>Responses</div>
+        </div>
+
+        {responses.map(([code, response], index) => (
+          <Response key={code} code={code} response={response} initialOpen={index === 0} />
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const Response = ({ code, response, initialOpen }: ResponseProps) => {
+  const [isOpen, setIsOpen] = useState(initialOpen);
+
+  return (
+    <>
+      <button
+        type="button"
+        className={cx(styles.item, styles.itemToggler)}
+        onClick={() => setIsOpen((open) => !open)}>
+        <div className={styles.subItem}>
+          <Tag statusCode={Number(code) as Tag.HttpResponseStatusCodes} />
+          <ChevronIcon
+            title=""
+            chevronDirection={isOpen ? 'up' : 'right'}
+            transition={{ duration: 0.35, ease: [0.55, 0, 0, 1] }}
+            className={styles.togglerIcon}
+          />
+        </div>
+        <div className={cx(styles.subItem, styles.subItemDescriptionWide)}>
+          {response.description || 'No description'}
+        </div>
+      </button>
+
+      <m.div
+        className={cx(styles.item, styles.itemContent)}
+        initial={isOpen ? 'expanded' : 'collapsed'}
+        animate={isOpen ? 'expanded' : 'collapsed'}
+        transition={transition}
+        variants={{
+          expanded: { opacity: 1, height: 'auto' },
+          collapsed: { opacity: 0, height: 0 },
+        }}>
+        <div className={styles.itemContentInner}>
+          {response.content ? (
+            <div className={styles.responseType}>
+              <p className={styles.responseTitle}>{response.description || 'Response body'}</p>
+              <div className={styles.responseTypeContent}>
+                {Object.entries(response.content).map(([media, content]) => {
+                  const schema = content.schema as SchemaObject;
+
+                  return <Schema key={media} schema={schema} />;
+                })}
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </m.div>
+    </>
+  );
+};
+
+const Schema = ({ schema }: { schema: SchemaObject }) => {
+  if (schema.type === 'array') {
+    return (
+      <>
+        <p className={styles.responseTypeTitle}>array of {`${schema.type}s`}</p>
+        <div className={styles.responseTypeContent}>
+          <Properties {...schema.items} />
+        </div>
+      </>
+    );
+  }
+
+  return <Properties {...schema} />;
+};
+
+const Properties = ({ properties, required, type }: SchemaObject) => {
+  return (
+    <>
+      {type ? <p className={styles.responseTypeTitle}>{type}</p> : null}
+
+      {properties ? (
+        <div className={styles.responseGrid}>
+          {Object.entries(properties).map(([name, property]) => (
+            <div key={name} className={styles.responseGridRow}>
+              <div className={styles.responseGridColumn}>{name}</div>
+              <div className={cx(styles.responseGridColumn, styles.responseGridColumnType)}>
+                {property.format || property.type}
+                {property.nullable && ' | null'}
+              </div>
+              <div className={styles.responseGridColumn}>
+                <RequiredTag isRequired={required?.includes(name)} />
+              </div>
+              <div className={cx(styles.responseGridColumn, styles.responseGridColumnRules)}>
+                {/* TODO: UI for validation rules */}
+                {property.maxLength ? `TODO: Validation rules` : null}
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </>
   );
 };
 
 type PropsRequestResponseProps = ApiOperation;
+
+interface ResponseProps {
+  code: string;
+  response: OpenAPIV3.ResponseObject;
+  initialOpen?: boolean;
+}
+
+// Replicate the OpenAPIV3.SchemaObject type but without ReferenceObject
+type SchemaObject = ArraySchemaObject | NonArraySchemaObject;
+
+interface ArraySchemaObject extends BaseSchemaObject {
+  type: OpenAPIV3.ArraySchemaObjectType;
+  items: SchemaObject;
+}
+
+interface NonArraySchemaObject extends BaseSchemaObject {
+  type?: OpenAPIV3.NonArraySchemaObjectType;
+}
+
+interface BaseSchemaObject extends OpenAPIV3.BaseSchemaObject {
+  additionalProperties?: boolean | SchemaObject;
+  properties?: { [name: string]: SchemaObject };
+  allOf?: SchemaObject[];
+  oneOf?: SchemaObject[];
+  anyOf?: SchemaObject[];
+}
