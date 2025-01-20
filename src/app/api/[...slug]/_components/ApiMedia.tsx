ff108f8c830a1fb155603d33e01882a49b4f206c
Tommy Bjerregaard <tommy@designsystems.international>
2025-01-20 14:44:28 +0100

    Added first draft of rendering the validation rules

--- src/app/api/[...slug]/_components/ApiMedia.tsx
+++ src/app/api/[...slug]/_components/ApiMedia.tsx
@@ -1,5 +1,6 @@
 import { RequestBodyObject, ResponseObject, SchemaObject } from '@/lib/swagger/types';
 import { cx } from 'class-variance-authority';
+import React from 'react';
 import { RequiredTag } from '../_components/RequireTag';
 
 import styles from './ApiMedia.module.css';
@@ -57,8 +58,8 @@ const Properties = ({ properties, required, type }: SchemaObject) => {
                 <RequiredTag isRequired={required?.includes(name)} />
               </div>
               <div className={cx(styles.responseGridColumn, styles.responseGridColumnRules)}>
-                {/* TODO: UI for validation rules */}
-                {property.maxLength ? `TODO: Validation rules` : null}
+                <ValidationRules schema={property} />
+                <pre style={{ border: '1px solid red' }}>{JSON.stringify(property, null, 2)}</pre>
               </div>
             </div>
           ))}
@@ -66,4 +67,100 @@ const Properties = ({ properties, required, type }: SchemaObject) => {
       ) : null}
     </>
   );
+};
+
+/*
+  Based on:
+  https://spec.openapis.org/oas/v3.0.3.html#properties
+  https://datatracker.ietf.org/doc/html/draft-wright-json-schema-validation-00#section-5
+*/
+const ValidationRules = ({ schema }: { schema: SchemaObject }) => {
+  const rules = Object.entries(schema).reduce<string[]>((acc, [key, value]) => {
+    if (!value) {
+      return acc;
+    }
+
+    switch (key) {
+      // Validation keywords for numeric instances
+      case 'multipleOf':
+        acc.push(`multiple of ${value}`);
+        break;
+      case 'maximum':
+        acc.push(`length ≤ ${value}${schema.exclusiveMaximum ? ' (exclusive)' : ''}`);
+        break;
+      case 'minimum':
+        acc.push(`length ≥ ${value}${schema.exclusiveMinimum ? ' (exclusive)' : ''}`);
+        break;
+
+      // Validation keywords for strings
+      case 'maxLength':
+        acc.push(`length ≤ ${value}`);
+        break;
+      case 'minLength':
+        acc.push(`length ≥ ${value}`);
+        break;
+      case 'pattern':
+        acc.push(`${value}`);
+        break;
+
+      // Validation keywords for arrays
+      case 'additionalItems':
+        if (value === false) {
+          acc.push('No additional items allowed');
+        }
+        break;
+      case 'maxItems':
+        acc.push(`length ≤ ${value}`);
+        break;
+      case 'minItems':
+        acc.push(`length ≥ ${value}`);
+        break;
+      case 'uniqueItems':
+        if (value === true) {
+          acc.push('items must be unique');
+        }
+        break;
+
+      // Validation keywords for objects
+      case 'maxProperties':
+        acc.push(`length ≤ ${value}`);
+        break;
+      case 'minProperties':
+        acc.push(`length ≥ ${value}`);
+        break;
+      case 'additionalProperties':
+        if (value === false) {
+          acc.push('no additional properties allowed');
+        }
+        break;
+
+      // Validation keywords for any instance type
+      case 'enum':
+        acc.push(`allowed values ${value.join(', ')}`);
+        break;
+      case 'allOf':
+        acc.push('Must match all schemas');
+        break;
+      case 'anyOf':
+        acc.push('Must match any schema');
+        break;
+      case 'oneOf':
+        acc.push('Must match exactly one schema');
+        break;
+      case 'not':
+        acc.push('Must not match schema');
+        break;
+      case 'format':
+        acc.push(`${value}`);
+        break;
+      case 'default':
+        acc.push(`defaults to ${value}`);
+        break;
+    }
+    return acc;
+  }, []);
+
+  if (rules.length) {
+    return rules.map((rule, index) => <React.Fragment key={index}>{rule}</React.Fragment>);
+  }
 };
