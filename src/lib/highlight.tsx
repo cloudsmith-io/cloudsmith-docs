0f11bd748f12be6f07fb9563c34f84734d21dbef
Jon Marsh <jmarsh@cloudsmith.io>
2025-01-28 13:59:03 +0000

    Add additional pages, menu items and syntax highlighters

--- src/lib/highlight.tsx
+++ src/lib/highlight.tsx
@@ -4,5 +4,5 @@ export const theme = 'material-theme-palenight';
 
 export const highlighter = createHighlighter({
   themes: [theme],
-  langs: ['js', 'jsx', 'ts', 'tsx', 'json', 'text', 'bash', 'yaml', 'ini'],
+  langs: ['js', 'jsx', 'ts', 'tsx', 'json', 'text', 'bash', 'yaml', 'ini', 'shell', 'powershell'],
 });
