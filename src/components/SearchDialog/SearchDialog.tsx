721abea883dd51f7ccd21f0945830959fcd99b9e
Rune Madsen <rune@runemadsen.com>
2025-01-29 13:07:17 +0100

    initial search results loaded

--- src/components/SearchDialog/SearchDialog.tsx
+++ src/components/SearchDialog/SearchDialog.tsx
@@ -6,8 +6,32 @@ import { SearchForm } from './SearchForm';
 import { SearchTrigger } from './SearchTrigger';
 
 import styles from './SearchDialog.module.css';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { performSearch } from '@/lib/search/server';
+import { debounce } from '@/lib/util';
+import { SearchResult } from '@/lib/search/types';
+
+const debouncedSearch = debounce(
+  async (term: string, sections: string[], setResults: Dispatch<SetStateAction<SearchResult[]>>) => {
+    const results = await performSearch(term, sections);
+    setResults(results);
+  },
+  300,
+);
 
 export const SearchDialog = () => {
+  const [term, setTerm] = useState('');
+  const [sections, setSections] = useState(['documentation', 'guides', 'api']);
+  const [results, setResults] = useState<SearchResult[]>([]);
+
+  useEffect(() => {
+    if (term === '') {
+      setResults([]);
+    } else {
+      debouncedSearch(term, sections, setResults);
+    }
+  }, [term, sections, setResults]);
+
   return (
     <RadixDialog.Root>
       <SearchTrigger />
@@ -18,7 +42,7 @@ export const SearchDialog = () => {
               <div className={styles.iconWrapper}>
                 <Icon name="search" className={styles.icon} title="" />
               </div>
-              <SearchForm value="" onChange={() => {}} />
+              <SearchForm value={term} onChange={setTerm} />
             </header>
             <VisuallyHidden>
               <RadixDialog.Close asChild>
@@ -26,7 +50,11 @@ export const SearchDialog = () => {
               </RadixDialog.Close>
               <RadixDialog.Title className={styles.title}>Search</RadixDialog.Title>
             </VisuallyHidden>
-            <div className={styles.main}>All the content here</div>
+            <div className={styles.main}>
+              {results.map((res) => (
+                <div key={res.path}>{res.title}</div>
+              ))}
+            </div>
           </RadixDialog.Content>
         </RadixDialog.Overlay>
       </RadixDialog.Portal>
