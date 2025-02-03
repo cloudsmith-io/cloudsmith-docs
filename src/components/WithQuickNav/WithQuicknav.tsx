4786e076f5321fa81efb0a869599ccc9c6404006
Tommy Bjerregaard <tommy@designsystems.international>
2025-02-03 11:07:37 +0100

    Added correct design to quick nav headings

--- src/components/WithQuickNav/WithQuicknav.tsx
+++ src/components/WithQuickNav/WithQuicknav.tsx
@@ -1,11 +1,19 @@
+import { Icon } from '@/icons';
 import { quickNavContentId } from '@/lib/constants/quickNav';
 import { QuickNav } from '../QuickNav';
+
 import styles from './WithQuicknav.module.css';
+
+/*
+  This component has a SVG symbol that is used to create a link icon for when hovering headings.
+  See next.config.mjs for the SVG symbol usage.
+*/
 
 const WithQuicknav = ({ children }: WithQuicknavProps) => {
   return (
     <main className={styles.root}>
       <article className={styles.content} id={quickNavContentId}>
+        <Icon name="action/link" as="symbol" title="" />
         {children}
       </article>
       <aside className={styles.nav}>
