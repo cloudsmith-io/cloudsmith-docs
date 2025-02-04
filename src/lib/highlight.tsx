ecf2d5a781b30c5fa3cb2af051f5102e690fe03a
Jon Marsh <jmarsh@cloudsmith.io>
2025-02-04 17:16:08 +0000

    Additional supported format pages and images

--- src/lib/highlight.tsx
+++ src/lib/highlight.tsx
@@ -4,5 +4,5 @@ export const theme = 'material-theme-palenight';
 
 export const highlighter = createHighlighter({
   themes: [theme],
-  langs: ['js', 'jsx', 'ts', 'tsx', 'json', 'text', 'bash', 'yaml', 'ini', 'shell', 'powershell', 'toml', 'r', 'groovy', 'kotlin', 'xml'],
+  langs: ['js', 'jsx', 'ts', 'tsx', 'json', 'text', 'bash', 'yaml', 'ini', 'shell', 'powershell', 'toml', 'r', 'groovy', 'kotlin', 'xml', 'scala'],
 });
