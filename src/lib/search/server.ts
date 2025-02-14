9903d9b7d107fbbaca7a77898d853fa377016d4d
Rune Madsen <rune@runemadsen.com>
2025-02-14 19:18:52 +0100

    kill eslint

--- src/lib/search/server.ts
+++ src/lib/search/server.ts
@@ -70,7 +70,9 @@ export const performSearch = async (
         .replace(/[^0-9a-z-A-Z \.\:]/g, '')
         .replace(/ +/, ' ');
 
-      const { content: _, ...rest } = item;
+      // Return everything but the content attribute
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { content, ...rest } = item;
 
       return {
         ...rest,
