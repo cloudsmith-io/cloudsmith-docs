cacd6d44372a8f09a9d33df506706d9bab600050
Pablo J. Lopez Zaldivar <pzaldivar@cloudsmith.io>
2025-08-18 14:00:23 +0200

    feat: Consolidate all 179 commits from staging (#177)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import LegacySiteBanner from '../components/LegacySiteBanner';
 import { Navbar } from '@/components';
 import { cx } from 'class-variance-authority';
 import type { Metadata } from 'next';
+import Script from "next/script";
 import { Suspense } from 'react';
 import { mdSystem, replica, replicaMono } from './_assets/fonts';
 import { NavigationEvents, NavigationProvider } from './navigation';
@@ -22,14 +24,31 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={cx(mdSystem.variable, replica.variable, replicaMono.variable)}>
       <body>
+        <LegacySiteBanner />
         <NavigationProvider>
           <Navbar />
           {children}
+          <noscript>
+            <img
+              src="https://simple.cloudsmith.com/noscript.gif?collect-dnt=true&hostname=docs.cloudsmith.com"
+              alt=""
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </noscript>
           <Suspense fallback={null}>
             <NavigationEvents />
           </Suspense>
         </NavigationProvider>
       </body>
+        <Script
+          strategy="afterInteractive"
+          async
+          defer
+          src="https://simple.cloudsmith.com/latest.js"
+          data-collect-dnt="true"
+          data-hostname="docs.cloudsmith.com"
+        />
     </html>
   );
 }
+
