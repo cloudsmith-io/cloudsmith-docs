7f55a0c7ed66be9128e12371b3e9544e1dea5ac6
Niall O'Brien <nobrien@cloudsmith.io>
2026-04-06 11:15:22 +0100

    fix: refactor SearchTrigger to include dynamic keyboard hints

--- src/components/SearchDialog/SearchTrigger/SearchTrigger.tsx
+++ src/components/SearchDialog/SearchTrigger/SearchTrigger.tsx
@@ -1,9 +1,12 @@
+'use client';
+
 import type { SearchTriggerVariant } from '../types';
 
 import { Trigger } from '@radix-ui/react-dialog';
 import { cx } from 'class-variance-authority';
 
 import { Icon } from '@/icons';
+import { useModifierKey, useShowKeyboardHints } from '@/lib/hooks';
 
 import styles from './SearchTrigger.module.css';
 
@@ -15,24 +18,31 @@ interface SearchTriggerProps {
   variant?: SearchTriggerVariant;
 }
 
-export const SearchTrigger = ({ className, theme = 'light', variant = 'inline' }: SearchTriggerProps) => (
-  <Trigger
-    aria-label="Search"
-    className={cx(
-      styles.root,
-      theme === 'dark' ? styles.themeDark : styles.themeLight,
-      styles[variant],
-      className,
-    )}>
-    <Icon name="search" className={styles.icon} title="" />
-    <span className={styles.placeholder}>Search</span>
-    <span className={styles.kbdHint}>
-      <kbd>
-        <span className={styles.kbdText}>Ctrl</span>
-      </kbd>
-      <kbd>
-        <span className={styles.kbdText}>K</span>
-      </kbd>
-    </span>
-  </Trigger>
-);
+export const SearchTrigger = ({ className, theme = 'light', variant = 'inline' }: SearchTriggerProps) => {
+  const modifierKey = useModifierKey();
+  const showHints = useShowKeyboardHints();
+
+  return (
+    <Trigger
+      aria-label="Search"
+      className={cx(
+        styles.root,
+        theme === 'dark' ? styles.themeDark : styles.themeLight,
+        styles[variant],
+        className,
+      )}>
+      <Icon name="search" className={styles.icon} title="" />
+      <span className={styles.placeholder}>Search</span>
+      {showHints && modifierKey && (
+        <span className={styles.kbdHint}>
+          <kbd>
+            <span className={styles.kbdText}>{modifierKey.symbol.replace('+', '')}</span>
+          </kbd>
+          <kbd>
+            <span className={styles.kbdText}>K</span>
+          </kbd>
+        </span>
+      )}
+    </Trigger>
+  );
+};
