cf8a556f8e6017ad10171e1b11c1cb63f51c2b4d
Niall O'Brien <nobrien@cloudsmith.io>
2026-04-08 10:16:29 +0100

    fix: improve background grid vignettes positioning and structure

--- src/components/BackgroundGrid/BackgroundGrid.tsx
+++ src/components/BackgroundGrid/BackgroundGrid.tsx
@@ -15,12 +15,12 @@ const BackgroundGrid = ({ className, children }: BackgroundGridProps) => {
     <div className={cx(styles.root, className)}>
       <div aria-hidden="true" className={styles.illustration}>
         <Grid />
+        <div aria-hidden="true" className={styles.topVignette}></div>
+        <div aria-hidden="true" className={styles.leftVignette}></div>
+        <div aria-hidden="true" className={styles.bottomVignette}></div>
+        <div aria-hidden="true" className={styles.rightVignette}></div>
       </div>
       <div className={styles.content}>{children}</div>
-      <div aria-hidden="true" className={styles.topVignette}></div>
-      <div aria-hidden="true" className={styles.leftVignette}></div>
-      <div aria-hidden="true" className={styles.bottomVignette}></div>
-      <div aria-hidden="true" className={styles.rightVignette}></div>
     </div>
   );
 };
