d6bae84e0fd958311304f105f6d32b495692084e
Niall O'Brien <nobrien@cloudsmith.io>
2026-05-01 16:56:52 +0100

    fix: update react-hotkeys-hook to version 5.3.0 and reorder eslint imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 // Copyright 2026 Cloudsmith Ltd
-import { defineConfig } from 'eslint/config';
+
 import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
 import nextTypeScript from 'eslint-config-next/typescript';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
